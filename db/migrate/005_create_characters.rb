3cc5b35725a73b2eeba7336360d2f914ce97a682
Andrew Ribas <andrew.j.ribas@gmail.com>
2020-08-12 17:30:47 +0000

    Automatically backed up by Learn

--- db/migrate/005_create_characters.rb
+++ db/migrate/005_create_characters.rb
@@ -2,7 +2,9 @@ class CreateCharacters < ActiveRecord::Migration[5.2]
 
   def change
     create_table :characters do |t|
-      t.
+      t.string :name
+      t.integer :actor_id
+      t.integer :show_id
     end
   end
 
