d7eff0fdb189b36f7daf8c03dbd7c1c4aaa48024
Andrew Ribas <andrew.j.ribas@gmail.com>
2020-08-12 17:39:28 +0000

    Automatically backed up by Learn

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -8,7 +8,7 @@ class Actor < ActiveRecord::Base
   end
 
   def list_roles
-    self.characters.collect{|c| c.name + " - " + c.show}
+    self.characters.collect{|c| c.name + " - " + c.show.name}
   end
 
 end
